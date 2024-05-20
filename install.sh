75bba4c81c8b8c5ed6892d52c3f60062cd15f2e3
spatiumstas <79056064+spatiumstas@users.noreply.github.com>
2024-05-20 12:50:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ REPO="KeenKit"
 SCRIPT="keenkit.sh"
 TMP_DIR="/tmp"
 OPT_DIR="/opt"
+
 opkg update
 opkg install curl
-
 curl -L -s "https://raw.githubusercontent.com/spatiumstas/$REPO/main/$SCRIPT" --output $TMP_DIR/$SCRIPT
 mv "$TMP_DIR/$SCRIPT" "$OPT_DIR/$SCRIPT"
 chmod +x $OPT_DIR/$SCRIPT
